d03e397c0d245bbd18e68e1eb73f6787cb0ce72d
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2025-02-13 19:57:09 -0600

    fix: drop matplotlib to prevent memory leaks

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     name="girder-sem-viewer",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    version="2.1.0",
+    version="2.1.1",
     description="Girder Plugin enabling preview of SEM data.",
     packages=find_packages(),
     include_package_data=True,
@@ -24,7 +24,7 @@ setup(
     ],
     python_requires=">=3.10",
     setup_requires=["setuptools-git"],
-    install_requires=["girder>=5.0.0a5.dev0", "pillow", "python-magic", "matplotlib"],
+    install_requires=["girder>=5.0.0a5.dev0", "pillow", "python-magic", "numpy"],
     entry_points={"girder.plugin": ["sem_viewer = girder_sem_viewer:SemViewerPlugin"]},
     zip_safe=False,
 )
